79d48a74a901873dbb44042e50fac9d846e11258
Yeolar <yeolar@qq.com>
2020-06-18 17:24:31 +0800

    update deps.sh

--- deps.sh
+++ deps.sh
@@ -1,7 +1,7 @@
 # git@branch or git@commit
 DEPS=(
 https://github.com/facebook/folly@v2018.08.06.00
-https://github.com/yeolar/wangle@v2018.08.06.00.fix:wangle
+https://github.com/yeolar/wangle@v2018.08.06.00.fix:"cmake -DCMAKE_PREFIX_PATH=../../usr/local ../wangle"
 )
 
 mkdir -p deps && cd deps
@@ -9,4 +9,4 @@ if [ ! -f dep-builder.py ]; then
     curl -L https://github.com/Yeolar/dep-builder/tarball/master | tar xz --strip 2 -C .
 fi
 
-python dep-builder.py - ${DEPS[*]}
+python dep-builder.py - "${DEPS[@]}"
