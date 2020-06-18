ef47f5c1c0f73fcc32a7d692f7bc18d31a9643dc
Yeolar <yeolar@qq.com>
2020-06-18 22:05:58 +0800

    update

--- deps.sh
+++ deps.sh
@@ -1,7 +1,10 @@
 # git@branch or git@commit
 DEPS=(
 https://github.com/facebook/folly@v2018.08.06.00
-https://github.com/yeolar/wangle@v2018.08.06.00.fix:"cmake -DCMAKE_PREFIX_PATH=../../usr/local ../wangle"
+https://github.com/Yeolar/wangle@v2018.08.06.00.fix:"cmake ../wangle"
+https://github.com/facebookresearch/faiss@v1.6.3:"./configure --without-cuda"
+https://github.com/Yeolar/accelerator@v2
+https://github.com/Yeolar/crystal
 )
 
 mkdir -p _deps && cd _deps
