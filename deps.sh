2166c8fa4050fc955c59772360b8902903667724
Yeolar <yeolar@qq.com>
2020-06-18 17:50:18 +0800

    update for build

--- deps.sh
+++ deps.sh
@@ -4,7 +4,7 @@ https://github.com/facebook/folly@v2018.08.06.00
 https://github.com/yeolar/wangle@v2018.08.06.00.fix:"cmake -DCMAKE_PREFIX_PATH=../../usr/local ../wangle"
 )
 
-mkdir -p deps && cd deps
+mkdir -p _deps && cd _deps
 if [ ! -f dep-builder.py ]; then
     curl -L https://github.com/Yeolar/dep-builder/tarball/master | tar xz --strip 2 -C .
 fi
