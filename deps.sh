944d6f6aa55b494bb67201b933b3f5531e3dc8e8
Yeolar <yeolar@qq.com>
2020-07-21 00:01:49 +0800

    add crystal dep

--- deps.sh
+++ deps.sh
@@ -4,6 +4,7 @@ https://github.com/yeolar/folly@v2018.08.06.00.fix
 https://github.com/Yeolar/wangle@v2018.08.06.00.fix:"cmake ../wangle"
 https://github.com/Yeolar/proxygen@v2018.08.06.00.fix:"cd proxygen && ./build.sh"
 https://github.com/Yeolar/accelerator@v2
+https://github.com/Yeolar/crystal@master
 )
 
 mkdir -p _deps && cd _deps
