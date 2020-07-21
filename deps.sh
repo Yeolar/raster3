77e0c1476fd4658e14bee7c5d6ccf9b58f83c6f2
Yeolar <yeolar@gmail.com>
2020-07-21 23:18:43 +0800

    update deps

--- deps.sh
+++ deps.sh
@@ -3,6 +3,8 @@ DEPS=(
 https://github.com/yeolar/folly@v2018.08.06.00.fix
 https://github.com/Yeolar/wangle@v2018.08.06.00.fix:"cmake ../wangle"
 https://github.com/Yeolar/proxygen@v2018.08.06.00.fix:"cd proxygen && ./build.sh"
+https://github.com/google/s2geometry@master:"cmake -DBUILD_SHARED_LIBS=OFF -DBUILD_EXAMPLES=OFF -DCMAKE_POSITION_INDEPENDENT_CODE=ON .."
+https://github.com/facebookresearch/faiss/archive/v1.6.3.zip@faiss-1.6.3:"./configure --without-cuda"
 https://github.com/Yeolar/accelerator@v2
 https://github.com/Yeolar/crystal@master
 https://github.com/Yeolar/inja@master
